fd0849ab3c5d3f07a455f0f2d06a53c651c72095
Dinesh7318 <dineshsiva7318@gmail.com>
2024-03-22 14:05:10 +0530

    commit

--- Contract/hardhat.config.js
+++ Contract/hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
     sepolia: {
       url: "https://eth-sepolia.g.alchemy.com/v2/GxA0-iF_T4fQIm1DJX1KVrjBoYcjp_aE",
       accounts: [
-        "YOUR_PRIVATE_KEY",
+        "4c6ae7fe27dc3e1838c4d21a4cd958ff60d4768b8edd2806f26b24468c34c214",
       ],
     },
   },
